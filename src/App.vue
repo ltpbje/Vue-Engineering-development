b3c2301d546aecd471e1006f1ad255e9f692385d
ltpbje <19836788252@163.com>
2023-08-29 13:07:33 +0800

    体验根组件开发

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,26 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div  class="box">
+    <div @click="fn">按钮</div>
   </div>
 </template>
-
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
+export default{
+  methods:{
+      fn(){
+        alert('点到我了');
+      }
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="less">
+  .box{
+      width: 400px;
+      height: 400px;
+      background-color: pink;
+      div{
+          height: 200px;
+          width: 200px;
+          background-color: skyblue;
+      }
+  } 
 </style>
